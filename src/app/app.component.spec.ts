6f99b197c7f0cdea93d51ad884c5ae4ec5489206
FG1907 <frknguener@gmail.com>
2024-06-27 19:59:29 +0200

    firstcommit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'reservation'`, () => {
+  it(`should have as title 'hotel-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('reservation');
+    expect(app.title).toEqual('hotel-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('reservation app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('hotel-app app is running!');
   });
 });
